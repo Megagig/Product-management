6b192ffb783dea3146e3ca0e7aa1e4cdcba1621d
Megagig <megagigsoftwaresolutions.com>
2024-10-21 13:33:06 +0100

    Successfully connected to the database

--- index.js
+++ index.js
@@ -3,21 +3,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+// Load environment variables from .env file
 dotenv.config();
 const productRoutes = require('./routes/productRoutes');
 
 // Create an Express application
 const app = express();
 
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI, {})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log('Error connecting to MongoDB:', err);
+  });
+
 // Define the port number
 const port = 3000;
 app.use(express.json()); // JSON Body parser middleware
 app.use('/api/v1/products', productRoutes);
-
-// Define a route for the root URL
-// app.get('/', (req, res) => {
-//   res.send('Hello World! Welcome to my Express server.');
-// });
 
 // Start the server
 app.listen(port, () => {
