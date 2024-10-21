1dc6d0500151c584238e1f625c5dbbbe0faa4f93
Megagig <megagigsoftwaresolutions.com>
2024-10-21 12:47:53 +0100

    Create the productroutes

---
+++ controllers/addProduct.js
@@ -0,0 +1,7 @@
+const addProduct = (req, res) => {
+  res.status(200).json({
+    message: 'This is a product route',
+  });
+};
+
+module.exports = addProduct;
--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 // Import the express module
 const express = require('express');
+const productRoutes = require('./routes/productRoutes');
 
 // Create an Express application
 const app = express();
 
 // Define the port number
 const port = 3000;
+app.use(express.json()); // JSON Body parser middleware
+app.use('/api/v1/products', productRoutes);
 
 // Define a route for the root URL
-app.get('/', (req, res) => {
-  res.send('Hello World! Welcome to my Express server.');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello World! Welcome to my Express server.');
+// });
 
 // Start the server
 app.listen(port, () => {
